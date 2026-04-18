fb1e2d18527d1c9d47a9826064f98bb7abbc213e
Peanutt42 <p3anutt42@gmail.com>
2026-04-18 19:02:40 +0200

    flake.nix: works for all systems now, not just x86_64-linux

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
     rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
@@ -10,36 +11,39 @@
     {
       self,
       nixpkgs,
+      flake-utils,
       rust-overlay,
     }:
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs {
-        inherit system;
-        overlays = [ rust-overlay.overlays.default ];
-      };
-      rustToolchain = pkgs.rust-bin.stable.latest.default;
-    in
-    {
-      packages.${system}.default = pkgs.rustPlatform.buildRustPackage {
-        pname = "git_progress_sync";
-        version = "0.2.1";
-        src = ./.;
-        cargoLock.lockFile = ./Cargo.lock;
-        nativeBuildInputs = [ pkgs.pkg-config ];
-        buildInputs = [ pkgs.openssl ];
-      };
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ rust-overlay.overlays.default ];
+        };
+        rustToolchain = pkgs.rust-bin.stable.latest.default;
+      in
+      {
+        packages.default = pkgs.rustPlatform.buildRustPackage {
+          pname = "git_progress_sync";
+          version = "0.2.1";
+          src = ./.;
+          cargoLock.lockFile = ./Cargo.lock;
+          nativeBuildInputs = [ pkgs.pkg-config ];
+          buildInputs = [ pkgs.openssl ];
+        };
 
-      overlays.default = final: prev: {
-        git_progress_sync = self.packages.${final.stdenv.hostPlatform.system}.default;
-      };
+        overlays.default = final: prev: {
+          git_progress_sync = self.packages.${final.stdenv.hostPlatform.system}.default;
+        };
 
-      devShells.${system}.default = pkgs.mkShell {
-        buildInputs = [
-          rustToolchain
-          pkgs.pkg-config
-          pkgs.openssl
-        ];
-      };
-    };
+        devShells.${system}.default = pkgs.mkShell {
+          buildInputs = [
+            rustToolchain
+            pkgs.pkg-config
+            pkgs.openssl
+          ];
+        };
+      }
+    );
 }
